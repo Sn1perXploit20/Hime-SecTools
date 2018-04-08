f5cf8d7ded4727f49832cc51a8d6a35660e971b8
Angga Saputra <ajulisaputra@gmail.com>
2018-04-08 23:26:48 +0800

    Add files via upload

---
+++ hime.sh
@@ -0,0 +1,377 @@
+#!/bin/bash
+# Special pentesting tools
+#  ___________________________________________
+# |                                           |
+# |   author : X20Coder                       |
+# |                                           |
+# |   contact me : fb.com/otakuhacking        |
+# |                                           |
+# |___________________________________________|
+#
+# klo mau recoder, sediakan sumbernya ea kentod  :>
+
+
+# warna 
+hitam='\e[30;1m'
+merah='\e[31;1m'
+hijau='\e[32;1m'
+kuning='\e[33;1m'
+biru='\e[34;1m'
+ungu='\e[35;1m'
+birumuda='\e[36;1m'
+putih='\e[37;1m'
+tutup='\e[39;1m'
+blink='\e[5m'
+
+# warna latar belakang
+KP='\e[43;1m'
+tutupBG='\e[49;1m'
+MBG = 'e\[42;1m'
+clear
+
+
+echo -e $merah
+cat << EOF
+
+# header
+
+     eee..eee..eeeeee...eeeeee...eeeee..eeeeee..eeeeeee..eeeeee.eeeeeee..
+     @@@::@@@:@@@@@@@@:@@@@@@@@:@@@@@@:@@@@@@@@:@@@@@@@@:@@@@@@:@@@@@@@@:
+     %%%--%%%-%%%--%%%-%%%--%%%-%%%----%%%--%%%-%%%--%%%-%%%----%%%--%%%-
+     +&&&&&&++++++&&&++&&&++&&&+&&&++++&&&++&&&+&&&++&&&+&&&&&++&&&&&&&++
+     *||||||*****|||***|||**|||*|||****|||**|||*|||**|||*|||||**||||||***
+     !!!==!!!===!!!====!!!==!!!=!!!====!!!==!!!=!!!==!!!=!!!====!!!=!!!==
+     :::##:::##:::::::#::::::::#::::::#::::::::#::::::::#::::::#:::##:::#
+     ...@@...@........@@......@@@.....@@......@@.......@@......@...@@...@
+
+                                                
+EOF
+echo -e $tutup $ungu'
+
+                            WELCOME TO X20CODER
+          ---------------------------------------------------------
+          ------>CREATE BY : --> Angga Saputra              <------
+          ------>PName     : --> Hime SecTools              <------
+          ------>Author    : --> X20 Family                 <------
+          ---------------------------------------------------------
+
+'
+
+# loading
+
+echo "";
+sleep 1.1
+echo -ne loading;
+sleep 1
+echo -ne '.';
+sleep 1.4
+echo -ne '.';
+sleep 1.2
+echo -ne '.';
+sleep 1.1
+echo -ne '.';
+sleep 1
+echo -ne '.';
+sleep 0.5
+echo -ne '.';
+sleep 0.4
+echo -ne '.';
+sleep 0.2
+echo -ne '.';
+sleep 0.1
+echo -ne "$hijau[done]";
+echo -e $tutup;
+sleep 2
+clear
+echo -e $KP'apakah anda ingin mendownload semua paket? [yes]:' $tutupBG
+read jembut 
+case $jembut in
+yes)
+echo '';
+echo ''
+echo -e $biru'Downloading...'
+echo '================================================'
+echo ""
+
+mkdir Hime-SecTools
+cd Hime-SecTools
+
+# download 
+
+git clone https://github.com/Screetsec/TheFatRat
+
+git clone https://github.com/DanMcInerney/shellshock-hunter
+
+git clone https://github.com/nethunteros/punters
+
+git clone https://github.com/m4ll0k/Infoga
+
+git clone https://github.com/1N3/Sn1per
+
+git clone https://github.com/DanMcInerney/shellshock-hunter-google
+
+git clone https://github.com/DanMcInerney/wifijammer
+
+git clone https://github.com/toxic-ig/Trity
+
+git clone https://github.com/darkoperator/dnsrecon
+
+git clone https://github.com/vk496/linset
+
+git clone https://github.com/ddos-defense/bohatei
+
+git clone https://github.com/desaster/kippo
+
+git clone https://github.com/the-c0d3r/sqli-scanner
+
+git clone https://github.com/epsylon/xsser
+
+git clone https://github.com/devttys0/binwalk
+
+git clone https://github.com/digininja/CeWL
+
+git clone https://github.com/NetZapper/NetZapper
+
+git clone https://github.com/PowerScript/KatanaFramework
+
+git clone https://github.com/Veil-Framework/Veil-Evasion
+
+git clone https://github.com/Dionach/CMSmap
+
+git clone https://github.com/dloss/python-pentest-tools
+
+git clone https://github.com/DoddyHackman/Arsenal_X
+
+git clone https://github.com/rezasp/joomscan
+
+git clone https://github.com/Hackplayers/4nonimizer
+
+git clone https://github.com/zigoo0/webpwn3r
+
+git clone https://github.com/DominikSchlecht/pyDirBuster
+
+git clone https://github.com/dotfighter/torshammer
+
+git clone https://github.com/dripcap/dripcap
+
+git clone https://github.com/padolsey/operative
+
+git clone https://github.com/EgeBalci/The-Eye
+
+git clone https://github.com/r00t-3xp10it/morpheus
+
+git clone https://github.com/EnableSecurity/wafw00f
+
+git clone https://github.com/endgameinc/binarypig
+
+git clone https://github.com/epinna/weevely3
+
+git clone https://github.com/Ettercap/ettercap
+
+git clone https://github.com/evilsocket/bettercap
+
+git clone https://github.com/rand0m1ze/ezsploit
+
+git clone https://github.com/ex0dus-0x/brut3k1t
+
+git clone https://github.com/ex0dus-0x/dedsploit
+
+git clone https://github.com/LionSec/xerosploit
+
+git clone https://github.com/fgeek/pyfiscan
+
+git clone https://github.com/foospidy/HoneyPy
+
+git clone https://github.com/frdmn/findmyhash
+
+git clone https://github.com/dana-at-cp/backdoor-apk
+
+git clone https://github.com/g0tmi1k/mpc
+
+git clone https://github.com/gabrielrcouto/reaver-wps
+
+git clone https://github.com/mempodippy/vlany.git
+
+git clone https://github.com/unix-thrust/beurk.git
+
+git clone https://github.com/nmap/nmap.git
+
+git clone https://github.com/leviathan-framework/leviathan.git
+
+git clone https://github.com/n1nj4sec/pupy/tree/master/client
+
+git clone https://github.com/valyala/goloris.git
+
+git clone https://github.com/nathanlopez/Stitch
+
+git clone https://github.com/radare/radare2.git
+
+git clone https://github.com/OffensivePython/Saddam.git
+
+git clone https://github.com/botherder/virustotal.git
+
+git clone https://github.com/Microsoft/binskim.git
+
+git clone https://github.com/0xd4d/dnSpy.git
+
+git clone https://github.com/lowleveldesign/wtrace.git
+
+git clone https://github.com/spesmilo/electrum.git
+
+git clone https://github.com/bitcoin-wallet/bitcoin-wallet.git
+
+git clone https://github.com/tuxotron/xvwa_lamp_container.git
+
+git clone https://github.com/JonathanSalwan/Triton.git
+
+git clone https://github.com/hypsurus/weeman.git
+
+git clone https://github.com/LandGrey/pydictor
+
+git clone https://github.com/krmaxwell/tinfoleak
+
+git clone https://github.com/secretsquirrel/fido.git
+
+git clone https://github.com/gentilkiwi/mimikatz.git
+
+git clone https://github.com/SecurityInnovation/AuthMatrix.git
+
+git clone https://github.com/akheron/jansson
+
+git clone https://github.com/wolf9466/cpuminer-multi.git
+
+git clone https://github.com/tsiv/ccminer-cryptonight.git
+
+git clone https://github.com/evilsocket/bettercap-proxy-modules.git
+
+git clone https://github.com/Raikia/FiercePhish.git
+
+git clone https://github.com/jekyc/wig
+
+git clone https://github.com/susmithHCK/torghost
+
+git clone https://github.com/i3visio/osrframework
+
+git clone https://github.com/xdavidhu/portSpider
+
+git clone https://github.com/onevcat/Kingfisher
+
+git clone https://github.com/adaptivethreat/Empire.git
+
+git clone https://github.com/gophish/gophish.git
+
+git clone https://github.com/securestate/king-phisher
+
+git clone https://github.com/golismero/golismero
+
+git clone https://github.com/DhavalKapil/icmptunnel.git
+
+git clone https://github.com/sqlmapproject/sqlmap.git
+
+git clone https://github.com/psypanda/hashID
+
+git clone https://github.com/TryGhost/Ghost
+
+git clone https://github.com/davidpepper/fierce-domain-scanner.git
+
+git clone https://github.com/Mebus/cupp.git
+
+git clone https://github.com/digininja/CeWL.git
+
+git clone https://github.com/iagox86/dnscat2.git
+
+git clone https://github.com/ryhanson/phishery.git
+
+git clone https://github.com/4w4k3/Umbrella
+
+git clone https://github.com/JusticeRage/Manalyze.git
+
+git clone git://git.kali.org/packages/exploitdb.git
+
+git clone https://github.com/trustedsec/social-engineer-toolkit.git
+
+git clone https://github.com/roobixx/cowpatty.git
+
+git clone https://github.com/hashcat/hashcat.git
+
+git clone https://github.com/laramies/theHarvester.git
+
+git clone https://github.com/secretsquirrel/the-backdoor-factory.git
+
+git clone https://github.com/EnableSecurity/wafw00f.git
+
+git clone https://github.com/stephenfewer/ReflectiveDLLInjection.git
+
+git clone https://github.com/kstaken/dockerfile-examples.git
+
+git clone https://github.com/xmendez/wfuzz.git
+
+git clone https://github.com/AlisamTechnology/ATSCAN
+
+git clone https://github.com/pxb1988/dex2jar.git
+
+git clone git://git.kali.org/packages/dirbuster.git
+
+git clone https://github.com/deltaxflux/fluxion.git
+
+git clone git://git.kali.org/packages/joomscan.git
+
+git clone https://github.com/hexploitable/MEMSCAN.git
+
+git clone https://github.com/hiddenillusion/AnalyzePDF.git
+
+git clone https://github.com/smalot/pdfparser.git
+
+git clone https://github.com/offensive-security/exploit-database/blob/master/searchsploit
+
+git clone https://github.com/jesparza/peepdf.git
+
+git clone https://github.com/reverse-shell/routersploit.git
+
+git clone https://github.com/1N3/MassBleed
+
+git clone https://github.com/google/sandbox-attacksurface-analysis-tools.git
+
+git clone https://github.com/sophron/wifiphisher.git
+
+git clone https://github.com/wpscanteam/wpscan.git
+
+git clone https://github.com/Yara-Rules/rules.git
+
+git clone https://github.com/Screetsec/Brutal
+
+git clone git://github.com/cuckoosandbox/cuckoo.git
+
+git clone https://github.com/juansacco/exploitpack.git
+
+git clone https://github.com/BlackArch/webshells.git
+
+git clone https://github.com/EgeBalci/HERCULES.git
+
+git clone https://github.com/jseidl/GoldenEye
+
+git clone https://github.com/lightos/credmap
+
+git clone https://github.com/EgeBalci/Cminer.git
+
+git clone https://github.com/P0cL4bs/WiFi-Pumpkin
+
+git clone https://github.com/googleinurl/SCANNER-INURLBR
+
+git clone https://github.com/EgeBalci/The-Eye.git
+
+git clone https://github.com/DanMcInerney/wifijammer.git
+
+git clone https://github.com/4shadoww/hakkuframework
+
+clear
+echo -e $hijau
+echo -e 'DAFTAR TOOLS'
+echo -e '================' $tutup
+echo ''
+
+# list / Daftar
+
+ls
+esac
+
